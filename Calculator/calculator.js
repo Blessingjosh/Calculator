78b5fef02b6bcfb9b223b93917bd8848726e092f
Blessingjosh <123566911+Blessingjosh@users.noreply.github.com>
2023-10-18 21:18:12 +0100

    Add files via upload

---
+++ Calculator/calculator.js
@@ -0,0 +1,11 @@
+function clearScreen(){
+  document.getElementById("result").value = ''
+}
+function display(value){
+  document.getElementById("result").value += value
+}
+function calculate(){
+  var q =  document.getElementById("result").value
+  p = eval(q)
+  document.getElementById("result").value = p
+}
